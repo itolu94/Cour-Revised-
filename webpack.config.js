d937bb73c85b025f461554b8273e93612ca29b00
Tolu Idowu <itolu94@gmail.com>
2017-08-12 12:51:04 -0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,31 @@
 module.exports = {
-  entry: [
-    './app/app.js'
-  ],
+
+  // This is the entry point or start of our react applicaton
+  entry: "./app/app.js",
+
+  // The plain compiled JavaScript will be output into this file
   output: {
-    path: __dirname,
-    publicPath: './public/',
-    filename: 'public/assets/javascript/bundle.js'
+    filename: "public/bundle.js"
   },
+
+  // This section desribes the transformations we will perform
   module: {
-    loaders: [{
-    include: /app/, 
-    //   exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['react', 'es2015']
+    loaders: [
+      {
+        // Only working with files that in in a .js or .jsx extension
+        test: /\.jsx?$/,
+        // Webpack will only process files in our app folder. This avoids processing
+        // node modules and server files unnecessarily
+        include: /app/,
+        loader: "babel",
+        query: {
+          // These are the specific transformations we'll be using.
+          presets: ["react", "es2015"]
+        }
       }
-    }]
+    ]
   },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
+  // This lets us debug our react code in chrome dev tools. Errors will have lines and file names
+  // Without this the console says all errors are coming from just coming from bundle.js
+  devtool: "eval-source-map"
 };
