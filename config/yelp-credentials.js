cc439281fd9f2fa9db7226649458e4896ae42473
Tolu Idowu <itolu94@gmail.com>
2017-10-16 22:26:46 -0400

    herok fix

--- config/yelp-credentials.js
+++ config/yelp-credentials.js
@@ -1,2 +1,2 @@
-exports.clientID =  '' || process.env.CLIENT_ID;
-exports.clientSecret =  '' || process.env.CLIENT_SECRET;
+exports.clientID =  process.env.CLIENT_ID  || '';
+exports.clientSecret =  process.env.CLIENT_SECRET || '';
